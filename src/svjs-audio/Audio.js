4b5bb1bf75c53fcbdcfd02e098c9efdfe0477ecd
Stefan Haack <shaack@gmail.com>
2018-08-11 15:39:17 +0200

    ios bug fix, see https://stackoverflow.com/questions/17892345/webkit-audio-distorts-on-ios-6-iphone-5-first-time-after-power-cycling

--- src/svjs-audio/Audio.js
+++ src/svjs-audio/Audio.js
@@ -6,7 +6,21 @@
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext
 
-const audioContext = new AudioContext()
+const desiredSampleRate = 44100
+// ios safari fix from https://github.com/Jam3/ios-safe-audio-context
+let audioContext = new AudioContext()
+if (/(iPhone|iPad)/i.test(navigator.userAgent) &&
+    audioContext.sampleRate !== desiredSampleRate) {
+    const buffer = audioContext.createBuffer(1, 1, desiredSampleRate)
+    const sound = audioContext.createBufferSource()
+    sound.buffer = buffer
+    sound.connect(audioContext.destination)
+    sound.start(0)
+    sound.disconnect()
+    audioContext.close().then(() => {
+        audioContext = new AudioContext()
+    })
+}
 const mainGainNode = audioContext.createGain()
 mainGainNode.gain.value = 1
 mainGainNode.connect(audioContext.destination)
